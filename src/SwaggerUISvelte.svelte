e35f5f9fed05d1f4f2b817f4cffe56565a503d61
cristianpb <felipebrokate@gmail.com>
2020-10-14 05:57:04 +0200

    ✨ Parse regex path parameters and header

--- src/SwaggerUISvelte.svelte
+++ src/SwaggerUISvelte.svelte
@@ -9,6 +9,7 @@
   // compatibility issues when importing module
   // import type { OpenAPIV3 } from "openapi-types";
   // let swagger: OpenAPIV3.Document = null;
+  import { compile } from "path-to-regexp";
 
   export let swaggerUrl = 'swagger-example.json';
 
@@ -60,24 +61,44 @@
     return swagger[division[0]][division[1]][division[2]]
   }
 
-  const handleRequest = async (route, method, methodId) => {
+  const handleRequest = async (route, method, methodId, paramDetails) => {
     const reqParams = Object.entries(requestParams).filter(x => x[0].indexOf(methodId) > -1)
     const params = {}
     if (reqParams.length > 0) {
-      reqParams.forEach(x => params[`${x[0].split("-")[2]}`] = x[1])
+      reqParams.forEach(x => {
+        const paramName = `${x[0].split("-")[2]}`
+        const paramIn = paramDetails.find(param => param.name === paramName).in
+        if (paramIn == 'query')  {
+          params[paramName] = x[1]
+        }
+      })
     }
-
+    const headers = { 'Content-Type': 'application/json' }
+    const inPath = {}
+    paramDetails.forEach(x => {
+      if (x.in === 'path') {
+        if ( reqParams.length > 0 && reqParams.find(param => param[0] === `${methodId}-${x.name}`)[1] ) {
+          inPath[x.name] = reqParams.find(param => param[0] === `${methodId}-${x.name}`)[1] // paramDetails.indexOf(param => param.name === x.name)params[x.name]
+        } else {
+          inPath[x.name] = null
+        }
+      }
+      if (x.in === 'header') {
+        if ( reqParams.length > 0 && reqParams.find(param => param[0] === `${methodId}-${x.name}`)[1] ) {
+          headers[x.name] = reqParams.find(param => param[0] === `${methodId}-${x.name}`)[1]
+        }
+      }
+    })
+    const toPath = compile(route, { encode: encodeURIComponent });
+    const encodedUri = toPath(inPath)
     const queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
-    const url = `${baseurl}${route}${queryString ? `?${queryString}` : ''}`
+    const url = `${baseurl}${encodedUri}${queryString ? `?${queryString}` : ''}`
     const response = await fetch(url, {
       method: method.toUpperCase(), // *GET, POST, PUT, DELETE, etc.
       // mode: 'cors', // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
+      headers: headers,
       redirect: 'follow', // manual, *follow, error
       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: requestBodyExample[methodId] // JSON.stringify(data) // body data type must match "Content-Type" header
@@ -256,7 +277,7 @@
                       </div>
                     </div>
                   {/if}
-                  <button class="rf-btn" on:click={() => handleRequest(method.route, method.method, method.id)}>
+                  <button class="rf-btn" on:click={() => handleRequest(method.route, method.method, method.id, method.parameters)}>
                     Execute
                   </button>
                   {#if responses[method['id']] }
